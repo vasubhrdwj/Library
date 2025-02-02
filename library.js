80e700c8848a763895646135487c26d440f41f3a
Vasu Bhardwaj <vasubhrdwj@gmail.com>
2025-02-02 18:56:22 +0530

    Added and styled heading

--- library.js
+++ library.js
@@ -1,3 +1,21 @@
-let a = document.querySelector(".test");
+const myLibrary = [];
 
-a.innerText = "Vasu";
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBooktoLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+addBooktoLibrary("Atomic Habits", "James Clear", 250, true);
+
+function displayBooks(){
+  for(let i=0; i <myLibrary.size(); i++){
+    
+  }
+}
